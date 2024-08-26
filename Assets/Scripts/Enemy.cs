28b730ef006ca175493d82f89a04aea4dcc96d36
shamblez-44 <engslevbenjamin@gmail.com>
2024-08-26 09:50:39 +0200

    Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed = 4.0f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y)*moveSpeed;
+        }
+    }
+}
